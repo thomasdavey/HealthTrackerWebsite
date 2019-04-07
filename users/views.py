000476fd1c9433bdeb5e6e4a4d0cd376107b4257
Jamie Greasley <ata17yhu@uea.ac.uk>
2019-04-07 22:52:02 +0100

    Modified encoding to stop quotation mark error (hopefully correctly this time)

--- users/views.py
+++ users/views.py
@@ -38,7 +38,7 @@ def register(request):
             message = render_to_string('account_activation_email.html', {
                 'user': user,
                 'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': account_activation_token.make_token(user),
             })
             user.email_user(subject, message)

535043e245cc4c6d24756ba0c343bb1604218d15
Amy Pryor <amy.a.p@hotmail.com>
2019-04-10 14:31:50 +0100

    changes to settings page + views.py

--- tracker/views.py
+++ tracker/views.py
@@ -2,6 +2,44 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from datetime import date
 
+accountDetails = [
+    {
+        'name': 'myname',
+        'age' : 'myage',
+        'weight' : 'myweight',
+        'height' : 'myheight',
+        'currentgoals' : 'mygoals'
+    }
+]
+
+healthData = [
+    {
+        'bmi': 'mybmi',
+        'idealweight' : 'myideal',
+        'targetcals': 'mycals',
+        'targetfat': 'myfat',
+        'targetcarbs' : 'mycarbs',
+        'targetprotein' : 'myprotein'
+    }
+]
+
+customFoods = [
+    {
+        'food': 'foodname'
+    }
+]
+
+customExercises = [
+    {
+        'exercise': 'exercisename'
+    }
+]
+
+completedGoals = [
+    {
+        'goal': 'mygoal'
+    }
+]
 
 @login_required()
 def home(request):
@@ -27,4 +65,11 @@ def groups(request):
 
 @login_required()
 def settings(request):
-    return render(request, 'settings.html', {'selected': 'settings'})
+    context = {
+        'details': accountDetails,
+        'health' : healthData,
+        'foods' : customFoods,
+        'exercises' : customExercises,
+        'goals' : completedGoals
+    }
+    return render(request, 'settings.html', context, {'selected': 'settings'})

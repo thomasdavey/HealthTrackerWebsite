2a8b59a0c82440afff7c936e359b588920c00ca3
Jamie Greasley <ata17yhu@uea.ac.uk>
2019-04-10 09:37:40 +0100

    Copied Java db structure to groupmember/ message models

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Group
+from .models import Message
+from .models import GroupMember
 
-admin.site.register(Group)
+admin.site.register(Message)
+admin.site.register(GroupMember)
--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-class Group(models.Model):
-    name = models.CharField(max_length=100)
+class Message(models.Model):
+    group_id = models.CharField(max_length=100, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    message = models.TextField(null=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+
+class GroupMember(models.Model):
+    group_id = models.CharField(max_length=100, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)

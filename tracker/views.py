89030e908ca777bbcc322bee3a91d147292f94bd
Thomas Davey <thomas.r.davey@uea.ac.uk>
2019-04-11 17:09:38 +0100

    Added lots of settings functionality.

--- tracker/views.py
+++ tracker/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
 from django.contrib import messages
 from datetime import date
-from users.forms import ProfileUpdateForm
+from users.forms import AccountUpdateForm ,ProfileUpdateForm
 from .forms import MessageForm
 from .models import Message
 
@@ -50,10 +50,20 @@ def groups(request):
 
 @login_required()
 def settings(request):
+    joined_date = request.user.date_joined
+    format_date = joined_date.strftime('%d/%m/%Y')
+    update_account_form = AccountUpdateForm(instance=request.user)
     update_password_form = PasswordChangeForm(user=request.user)
-    update_profile_form = ProfileUpdateForm()
-    modal = 'None'
+    update_profile_form = ProfileUpdateForm(instance=request.user.profile)
+    modal = 'none'
     if request.method == 'POST':
+        if 'update_account' in request.POST:
+            update_account_form = AccountUpdateForm(data=request.POST, instance=request.user)
+            modal = 'update_account'
+            if update_account_form.is_valid():
+                update_account_form.save()
+                messages.success(request, f'Account details successfully updated!')
+                return redirect('tracker-settings')
         if 'update_password' in request.POST:
             update_password_form = PasswordChangeForm(data=request.POST, user=request.user)
             modal = 'update_password'
@@ -63,7 +73,8 @@ def settings(request):
                 messages.success(request, f'Password successfully changed!')
                 return redirect('tracker-settings')
         if 'update_profile' in request.POST:
-            update_password_form = ProfileUpdateForm(data=request.POST)
+            update_profile_form = ProfileUpdateForm(data=request.POST, files=request.FILES, instance=request.user.profile)
+            print(request.FILES)
             modal = 'update_profile'
             if update_profile_form.is_valid():
                 update_profile_form.save()
@@ -71,6 +82,8 @@ def settings(request):
                 return redirect('tracker-settings')
     context = {
         'selected': 'Settings',
+        'format_date': format_date,
+        'update_account_form': update_account_form,
         'update_password_form': update_password_form,
         'update_profile_form': update_profile_form,
         'modal': modal
--- users/forms.py
+++ users/forms.py
@@ -56,6 +56,19 @@ class AccountUpdateForm(forms.ModelForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
+    image = forms.FileField(required=False, widget=forms.FileInput)
+
+    def clean_birth_date(self):
+        birth_date = self.cleaned_data['birth_date']
+        verify_age = date.today()
+        verify_age = verify_age.replace(year=verify_age.year-16)
+
+        if birth_date > date.today():
+            raise forms.ValidationError("Enter a valid date.")
+        elif birth_date > verify_age:
+            raise forms.ValidationError("You must be at least 16 years old to use Longevity.")
+
+        return birth_date
 
     class Meta:
         model = Profile
@@ -63,6 +76,13 @@ class ProfileUpdateForm(forms.ModelForm):
 
 
 class WeightGoalUpdateForm(forms.ModelForm):
+
+    def clean_target_date(self):
+        target_date = self.cleaned_data['target_date']
+
+        if target_date <= date.today():
+            raise forms.ValidationError("Enter a valid date.")
+        return target_date
 
     class Meta:
         model = WeightGoal

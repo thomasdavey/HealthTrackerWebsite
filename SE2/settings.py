c2fb5e0267aa5b9d8333ee4ed25cdd806bdefe54
Thomas Davey <thomas.r.davey@uea.ac.uk>
2019-05-13 20:07:15 +0100

    Added CSS comments. Fixed JavaScript problem with mobile site. Fixed author name of registration email.

--- SE2/settings.py
+++ SE2/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+ALLOWED_HOSTS = ['*']
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ('static',)
 
